b288b266e8e61d0328e4471e8edea267650cacde
Tawmy <git@tawmy.dev>
2026-04-27 23:01:22 +0200

    Move to Xunit 3, remove indentation from xmldocs

--- NetStone.Api.Sdk.Test/EnvironmentVariables.cs
+++ NetStone.Api.Sdk.Test/EnvironmentVariables.cs
@@ -3,38 +3,38 @@ namespace NetStone.Api.Sdk.Test;
 public static class EnvironmentVariables
 {
     /// <summary>
-    ///     Root URI for the NetStone API.
+    /// Root URI for the NetStone API.
     /// </summary>
     /// <remarks>
-    ///     Routes will be appended to this URI.
+    /// Routes will be appended to this URI.
     /// </remarks>
     public const string ApiBaseAddress = "API_BASE_ADDRESS";
 
     /// <summary>
-    ///     Authority for OAuth Client Credentials flow.
+    /// Authority for OAuth Client Credentials flow.
     /// </summary>
     public const string AuthAuthority = "AUTH_AUTHORITY";
 
     /// <summary>
-    ///     OAuth 2.0 client ID.
+    /// OAuth 2.0 client ID.
     /// </summary>
     public const string AuthClientId = "AUTH_CLIENT_ID";
 
     /// <summary>
-    ///     OAuth 2.0 signed JWT client authentication. Path to certificate.
+    /// OAuth 2.0 signed JWT client authentication. Path to certificate.
     /// </summary>
     public const string AuthClientSignedJwtCertificate = "AUTH_CLIENT_SIGNED_JWT_CERTIFICATE";
 
     /// <summary>
-    ///     OAuth 2.0 signed JWT client authentication. Path to private key.
+    /// OAuth 2.0 signed JWT client authentication. Path to private key.
     /// </summary>
     public const string AuthClientSignedJwtPrivateKey = "AUTH_CLIENT_SIGNED_JWT_PRIVATE_KEY";
 
     /// <summary>
-    ///     OAuth 2.0 client scopes.
+    /// OAuth 2.0 client scopes.
     /// </summary>
     /// <remarks>
-    ///     Scopes are divided with spaces.
+    /// Scopes are divided with spaces.
     /// </remarks>
     public const string AuthScopes = "AUTH_SCOPES";
 }
